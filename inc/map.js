ad1d48893f2bb4f5f4486edf30689d8ba8efa856
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2016-08-05 08:54:05 +0200

    Initialize map

---
+++ inc/map.js
@@ -0,0 +1,13 @@
+var map;
+
+register_hook('init', function() {
+  map = L.map('map').setView([48.202, 16.338], 15);
+
+  var osm_mapnik = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }
+  );
+  osm_mapnik.addTo(map);
+});
--- index.php
+++ index.php
@@ -9,7 +9,10 @@
     <?php print modulekit_include_js(); /* prints all js-includes */ ?>
     <?php print modulekit_include_css(); /* prints all css-includes */ ?>
     <?php print_add_html_headers(); /* print additional html headers */ ?>
+    <link rel="stylesheet" href="https://npmcdn.com/leaflet@1.0.0-rc.2/dist/leaflet.css" />
+    <script src="https://npmcdn.com/leaflet@1.0.0-rc.2/dist/leaflet.js"></script>
   </head>
   <body>
+  <div id='map'></div>
   </body>
 </html>
