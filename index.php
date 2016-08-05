efb207172c562e47a641483bf8b571201e20cb5d
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2016-08-05 12:38:50 +0200

    Index: mobile device support

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <?php print modulekit_include_js(); /* prints all js-includes */ ?>
     <?php print modulekit_include_css(); /* prints all css-includes */ ?>
     <?php print_add_html_headers(); /* print additional html headers */ ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://npmcdn.com/leaflet@1.0.0-rc.2/dist/leaflet.css" />
     <script src="https://npmcdn.com/leaflet@1.0.0-rc.2/dist/leaflet.js"></script>
     <script src="lib/async.js"></script>
