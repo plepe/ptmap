83b8d368286080616a6c54622c42e789b3e1565f
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2016-08-05 10:31:46 +0200

    Show several routes

--- inc/index.js
+++ inc/index.js
@@ -1,8 +1,11 @@
 window.onload = function() {
   call_hooks("init");
 
-  http_load("910885.json", null, null, function(err, data) {
-    var route = new OSMRoute("r" + data.id, data);
-    route.render();
-  });
+  var route_ids = [ '1306732', '1306733', '910885', '910886' ];
+  for(var i = 0; i < route_ids.length; i++) {
+    http_load(route_ids[i] + '.json', null, null, function(err, data) {
+      var route = new OSMRoute("r" + data.id, data);
+      route.render();
+    });
+  }
 }
