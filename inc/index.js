76a02742b278f7dbb60033d8d5bd787ae73f7566
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2016-08-05 10:29:58 +0200

    OSMRoute: render()

--- inc/index.js
+++ inc/index.js
@@ -2,7 +2,7 @@ window.onload = function() {
   call_hooks("init");
 
   http_load("910885.json", null, null, function(err, data) {
-    console.log(data);
     var route = new OSMRoute("r" + data.id, data);
+    route.render();
   });
 }
