7c297d7b2e4e27caa765ea5d2f07f2d69ea84eac
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2016-08-05 08:00:24 +0200

    Create configuration files

---
+++ modulekit.php
@@ -0,0 +1,17 @@
+<?php
+$name = "PT-Map";
+$id = "ptmap";
+$depend = array(
+);
+
+$include = array(
+  'php' => array(
+    'inc/*.php' // automatically include all files in inc-directory
+  ),
+  'js' => array(
+    'inc/*.js' // automatically include all files in inc-directory
+  ),
+  'css' => array(
+    'style.css' // include style.css
+  )
+);
