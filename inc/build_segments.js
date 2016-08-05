038cb74d5b9084602323bf4ac62e5ef7c5e01308
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2016-08-05 20:14:38 +0200

    Use objects instead of arrays

--- inc/build_segments.js
+++ inc/build_segments.js
@@ -26,44 +26,52 @@ function build_segments(routes) {
     var parts = route_parts[route.id];
     var last_links = null;
     var current_segment = {
+      id: segments.length,
       ways: []
     };
 
     for(var j = 0; j < parts.length; j++) {
       var part = parts[j];
       var links = ways[part.member.ref].links;
+
+      // if current way has not already been assigned to a segment
       if(!ways[part.member.ref].segment) {
+        // first way of a route
+        if(last_links !== null) {
+          var match = true;
 
-      if(last_links === null) {
-      }
-      else {
-        var match = true;
-
-        for(var k in links) {
-          if(k in last_links) {
+          for(var k in links) {
+            if(k in last_links) {
+            }
+            else {
+              match = false;
+            }
           }
-          else {
-            match = false;
+          for(var k in last_links) {
+            if(k in links) {
+            }
+            else {
+              match = false;
+            }
+          }
+
+          // no match -> create new segment
+          if(!match) {
+            segments.push(current_segment);
+            current_segment = {
+              id: segments.length,
+              ways: []
+            };
           }
         }
-        for(var k in last_links) {
-          if(k in links) {
-          }
-          else {
-            match = false;
-          }
-        }
 
-        if(!match) {
-          segments.push(current_segment);
-          current_segment = {
-            ways: []
-          };
-        }
-      }
-
-      current_segment.ways.push([ part.member, links, part.link.dir ]);
-      ways[part.member.ref].segment = current_segment;
+        current_segment.ways.push({
+          id: part.member.ref,
+          way: part.member,
+          links: links,
+          dir: part.link.dir
+        });
+        ways[part.member.ref].segment = current_segment;
       }
 
       last_links = links;
@@ -79,12 +87,12 @@ function build_segments(routes) {
     for(var j = 0; j < segment.ways.length; j++) {
       var way = segment.ways[j];
 
-      for(var k = 0; k < way[0].geometry.length; k++) {
-        var g = way[0].geometry[k];
+      for(var k = 0; k < way.way.geometry.length; k++) {
+        var g = way.way.geometry[k];
         line.push([ g.lat, g.lon ]);
       }
     }
 
-    L.polyline(line, { color: 'red'}).addTo(map).bindPopup("<pre>" + JSON.stringify(segment.ways[0][1], null, '    '));
+    L.polyline(line, { color: 'red'}).addTo(map).bindPopup("<pre>" + JSON.stringify(segment.ways[0].links, null, '    '));
   }
 }
