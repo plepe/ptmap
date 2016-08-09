0288e1f2db3ca6a7f671d00d8fadb1691309ffec
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2016-08-09 20:09:04 +0200

    check_update_map(): defer update while another update is active

--- inc/check_update_map.js
+++ inc/check_update_map.js
@@ -1,8 +1,18 @@
 var current_route_ids = {};
 var current_route_count = 0;
 var current_sections = [];
+var check_update_map_active = false;
+var check_update_map_requested = false;
 
 function check_update_map() {
+  if(check_update_map_active) {
+    check_update_map_requested = true;
+    return;
+  }
+
+  check_update_map_active = true;
+  check_update_map_requested = false;
+
   get_routes(function(err, routes) {
 
     if(routes.length != current_route_count) {
@@ -24,8 +34,13 @@ function check_update_map() {
 	  change = true;
       }
 
-      if(!change)
+      if(!change) {
+	check_update_map_active = false;
+	if(check_update_map_requested)
+	  check_update_map();
+
 	return;
+      }
 
       current_route_count = routes.length;
     }
@@ -36,6 +51,10 @@ function check_update_map() {
       }
 
       current_sections = sections;
+
+      check_update_map_active = false;
+      if(check_update_map_requested)
+	check_update_map();
     });
   });
 }
