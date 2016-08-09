97721c510e2856143cc7e1817e14de1929aa6766
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2016-08-09 20:02:23 +0200

    check_update_map(): if route list is unmodified, do nothing

--- inc/check_update_map.js
+++ inc/check_update_map.js
@@ -1,7 +1,35 @@
+var current_route_ids = {};
+var current_route_count = 0;
 var current_sections = [];
 
 function check_update_map() {
   get_routes(function(err, routes) {
+
+    if(routes.length != current_route_count) {
+      current_route_ids = {};
+
+      for(var i = 0; i < routes.length; i++) {
+	current_route_ids[routes[i].id] = true;
+      }
+
+      current_route_count = routes.length;
+    }
+    else {
+      var new_route_ids = {};
+      var change = false;
+
+      for(var i = 0 ; i < routes.length; i++) {
+	new_route_ids[routes[i].id] = true;
+	if(!(routes[i].id in current_route_ids))
+	  change = true;
+      }
+
+      if(!change)
+	return;
+
+      current_route_count = routes.length;
+    }
+
     update_map(routes, function(err, sections) {
       for(var i = 0; i < current_sections.length; i++) {
 	current_sections[i].remove();
