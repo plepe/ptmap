8f505c45093ef94f21327f00724b70236b2c806d
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2016-08-09 18:28:09 +0200

    get_routes(): move code from index.js

---
+++ inc/get_routes.js
@@ -0,0 +1,17 @@
+function get_routes(callback) {
+  http_load(
+    'cache.php', // 'https://www.overpass-api.de/api/interpreter',
+    null,
+    "[out:json][bbox:48.194271721398096,16.3236665725708,48.20379718953509,16.35585308074951];relation[route=bus];out meta geom;",
+    function(err, results) {
+      var routes = [];
+      for(var i = 0; i < results.elements.length; i++) {
+	var data = results.elements[i];
+	var ob = create_osm_object(data);
+	routes.push(ob);
+      }
+
+      callback(null, routes);
+    }
+  );
+}
--- inc/index.js
+++ inc/index.js
@@ -1,20 +1,8 @@
 window.onload = function() {
   call_hooks("init");
 
-  http_load(
-    'cache.php', // 'https://www.overpass-api.de/api/interpreter',
-    null,
-    "[out:json][bbox:48.194271721398096,16.3236665725708,48.20379718953509,16.35585308074951];relation[route=bus];out meta geom;",
-    function(err, results) {
-      var routes = [];
-      for(var i = 0; i < results.elements.length; i++) {
-	var data = results.elements[i];
-	var ob = create_osm_object(data);
-	routes.push(ob);
-      }
-
-      update_map(routes, function() {
-      });
-    }
-  );
+  get_routes(function(err, routes) {
+    update_map(routes, function() {
+    });
+  });
 }
