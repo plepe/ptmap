a9c9514329049194264d1feacd3ebe964283189b
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2016-08-23 10:16:47 +0200

    convert_to_turf(): detect type of bounds

--- inc/convert_to_turf.js
+++ inc/convert_to_turf.js
@@ -1,12 +1,16 @@
 function convert_to_turf(bounds) {
-  var sw = bounds.getSouthWest();
-  var ne = bounds.getNorthEast();
+  if("getSouthWest" in bounds) {
+    var sw = bounds.getSouthWest();
+    var ne = bounds.getNorthEast();
 
-  return turf.polygon([[
-    [ sw.lng, sw.lat ],
-    [ sw.lng, ne.lat ],
-    [ ne.lng, ne.lat ],
-    [ ne.lng, sw.lat ],
-    [ sw.lng, sw.lat ]
-  ]]);
+    return turf.polygon([[
+      [ sw.lng, sw.lat ],
+      [ sw.lng, ne.lat ],
+      [ ne.lng, ne.lat ],
+      [ ne.lng, sw.lat ],
+      [ sw.lng, sw.lat ]
+    ]]);
+  }
+
+  return bounds;
 }
