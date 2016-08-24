ba31421318e4beb7c288ea010b794b014eb25033
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2016-08-24 15:12:48 +0200

    OSMRoute: re-activate processing of stop nodes

--- inc/OSMRoute.js
+++ inc/OSMRoute.js
@@ -24,42 +24,64 @@ OSMRoute.prototype.route_parts = function(bounds, callback) {
   this.route_parts_index = {};
 
   var way_list = [];
-  var index_list = [];
-  var index = 0;
+  var way_index_list = [];
+  var way_index = 0;
+
+  var node_index_list = [];
+  var node_list = [];
+  var node_index = 0;
   var init = false;
   if(!this._route_parts) {
     this.route_parts_roles = [];
     this.route_parts_way = [];
     this._route_parts = [];
+    this._stops = [];
+    this._route_stops_ob = [];
+    this._route_stops_ids = [];
+    this._route_stops_roles = [];
     init = true;
   }
 
   for(var i = 0; i < this.data.members.length; i++) {
     var member = this.data.members[i];
 
-    if(member.type != 'way')
-      continue;
-    if([ '', 'forward', 'backward'].indexOf(member.role) == -1)
-      continue;
+    if(member.type == 'way' && [ '', 'forward', 'backward'].indexOf(member.role) != -1) {
+      if(init) {
+	this.route_parts_roles.push(member.role);
+	this.route_parts_way.push(undefined);
+	this._route_parts.push(false);
+      }
 
-    if(init) {
-      this.route_parts_roles.push(member.role);
-      this.route_parts_way.push(undefined);
-      this._route_parts.push(false);
+      if(!this._route_parts[way_index]) {
+	way_list.push('w' + member.ref);
+	way_index_list.push(way_index);
+      }
+
+      way_index++;
     }
 
-    if(!this._route_parts[index]) {
-      way_list.push('w' + member.ref);
-      index_list.push(index);
-    }
+    if(member.type == 'node' && member.role == 'stop') {
+      if(init) {
+        this._route_stops_roles.push(member.role);
+        this._route_stops_ob.push(undefined);
+        this._route_stops_ids.push('n' + member.ref);
+        this._stops.push(false);
+      }
 
-    index++;
+      if(!this._stops[node_index]) {
+        node_list.push('n' + member.ref);
+        node_index_list.push(node_index);
+      }
+
+      node_index++;
+    }
   }
 
-  if(!way_list.length)
-    return async.setImmediate(function() {
+  if(!way_list.length && !node_list.length) {
+    async.setImmediate(function() {
       callback(null, this._route_parts);
     }.bind(this));
+  }
 
   if(!this.possible_bounds.is_possible(bounds))
     return async.setImmediate(function() {
@@ -68,27 +90,37 @@ OSMRoute.prototype.route_parts = function(bounds, callback) {
 
   this.possible_bounds.add_inner_bounds(bounds);
 
+  if(node_list.length)
+  overpass_get(node_list, {
+      call_ordered: true,
+      priority: 0 + this.priority
+    }, function(node_index_list, err, ob, i) {
+      var route_index = node_index_list[i];
+      this._route_stops_ob[route_index] = ob;
+
+      this.route_part_check_stop(route_index);
+    }.bind(this, node_index_list),
+    function() {
+      // TODO: make sure that both final_callbacks (way && node) are called.
+    }.bind(this)
+  );
+
+  if(way_list.length)
   overpass_get(way_list, {
       bbox: bounds,
       priority: 1 + this.priority
     },
     function(index_list, err, ob, i) {
-      var route_index = index_list[i];
+      var route_index = way_index_list[i];
       this.route_parts_way[route_index] = ob;
 
       if(ob === false)
         return;
 
       this.route_part_check_way(route_index);
-    }.bind(this, index_list),
+    }.bind(this, way_index_list),
   function() {
-//    this._route_parts_stops(result, this.route_parts_index, function(err, result) {
-//      this._route_parts = result;
-//
-//      shared_route_sections_add_route(this, this._route_parts, function() {
-        callback(null, this._route_parts);
-//      });
-//    }.bind(this));
+    callback(null, this._route_parts);
   }.bind(this));
 }
 
@@ -200,99 +232,94 @@ OSMRoute.prototype.route_part_check_way = function(route_index) {
     this.route_part_check_way(route_index + 1);
   }
 
+  for(var i = 0; i < ob.nodes.length; i++) {
+    var node = ob.nodes[i];
+    var node_index = null;
+
+    if((node_index = this._route_stops_ids.indexOf('n' + node)) != -1)
+      this.route_part_check_stop(node_index);
+  }
+
   result.shared_route_way.update();
 }
 
-OSMRoute.prototype._route_parts_stops = function(route_parts, route_parts_index, callback) {
-  var last_route_part_index = 0;
-  this._stops = [];
+OSMRoute.prototype.route_part_check_stop = function(route_index) {
+  var ob = this._route_stops_ob[route_index];
+  var role = this._route_stops_roles[route_index];
+  var last_route_part_index = 0; // TODO: read from prev. stop
 
-  var node_list = [];
-  var node_roles = [];
-  for(var i = 0; i < this.data.members.length; i++) {
-    var member = this.data.members[i];
-
-    if(member.type != 'node')
-      continue;
-    if(member.role != 'stop')
-      continue;
-
-    node_list.push('n' + member.ref);
-    node_roles.push(member.role);
+  if(!ob) {
+    console.log('ob not loaded yet');
+    return;
   }
 
-  overpass_get(node_list, {
-      call_ordered: true,
-      priority: 0 + this.priority
-    }, function(node_roles, err, ob, route_index) {
-      var node_ref;
+  var node_ref;
 
-      if(node_ref = this.node_index[ob.id.substr(1)]) {
-	var matching_route_parts_index = null;
-	var matching_route_parts_indexes = route_parts_index[node_ref.way.id];
-	for(var i = 0; i < matching_route_parts_indexes.length; i++) {
-	  if(matching_route_parts_indexes[i] >= last_route_part_index) {
-	    matching_route_parts_index = matching_route_parts_indexes[i];
-	  }
-	}
-
-	route_parts[matching_route_parts_index].link.stops.push({
-	  ob: ob,
-	  node_index: node_ref.index
-	});
-
-	this._stops.push({
-	  ob: ob,
-	  route_parts_index: matching_route_parts_index,
-	  node_index: node_ref.index
-	});
+  if(node_ref = this.node_index[ob.id.substr(1)]) {
+    var matching_route_parts_index = null;
+    var matching_route_parts_indexes = this.route_parts_index[node_ref.way.id];
+    for(var i = 0; i < matching_route_parts_indexes.length; i++) {
+      if(matching_route_parts_indexes[i] >= last_route_part_index) {
+	matching_route_parts_index = matching_route_parts_indexes[i];
       }
-      else {
-	var node_geo = ob.GeoJSON();
-	var matching_point = null;
-	var matching_distance = null;
-	var matching_route_parts_index = null;
+    }
 
-	for(var i = 0; i < route_parts.length; i++) {
-	  var point = turf.pointOnLine(route_parts[i].member.GeoJSON(), node_geo);
-	  var distance = turf.distance(node_geo, point);
+    this._route_parts[matching_route_parts_index].stops.push({
+      ob: ob,
+      node_index: node_ref.index
+    });
 
-	  if(matching_distance === null || distance < matching_distance) {
-	    matching_distance = distance;
-	    matching_point = point;
-	    matching_route_parts_index = i;
-	  }
-	}
+    this._stops.push({
+      ob: ob,
+      route_parts_index: matching_route_parts_index,
+      node_index: node_ref.index
+    });
+  }
+  else {
+    var node_geo = ob.GeoJSON();
+    var matching_point = null;
+    var matching_distance = null;
+    var matching_route_parts_index = null;
 
-	// matching point on route must be closer than 100m
-	if(matching_distance !== null && matching_distance < 0.1) {
-	  this._stops.push({
-	    ob: ob,
-	    route_parts_index: matching_route_parts_index,
-	    geometry: {
-	      lon: matching_point.geometry.coordinates[0],
-	      lat: matching_point.geometry.coordinates[1]
-	    }
-	  });
+    for(var i = 0; i < this._route_parts.length; i++) {
+      if(!this._route_parts[i])
+        continue;
 
-	  route_parts[matching_route_parts_index].link.stops.push({
-	    ob: ob
-	  });
+      var point = turf.pointOnLine(this._route_parts[i].way.GeoJSON(), node_geo);
+      var distance = turf.distance(node_geo, point);
 
-	  this.errors.push('Stop ' + ob.id + ' not connected to route way, found nearest location (' + (matching_distance * 1000) + 'm)');
-	}
-	else {
-	  this._stops.push({
-	    ob: ob
-	  });
-
-	  this.errors.push('Stop ' + ob.id + ' not connected to route way, could not find nearest location (not rendered!)');
-	}
+      if(matching_distance === null || distance < matching_distance) {
+	matching_distance = distance;
+	matching_point = point;
+	matching_route_parts_index = i;
       }
-  }.bind(this, node_roles),
-  function(callback, err) {
-    callback(null, route_parts);
-  }.bind(this, callback));
+    }
+
+    // matching point on route must be closer than 100m
+    if(matching_distance !== null && matching_distance < 0.1) {
+      this._stops.push({
+	ob: ob,
+	route_parts_index: matching_route_parts_index,
+	geometry: {
+	  lon: matching_point.geometry.coordinates[0],
+	  lat: matching_point.geometry.coordinates[1]
+	}
+      });
+
+      this._route_parts[matching_route_parts_index].stops.push({
+	ob: ob
+      });
+
+      this.errors.push('Stop ' + ob.id + ' not connected to route way, found nearest location (' + (matching_distance * 1000) + 'm)');
+    }
+    else {
+      this._stops.push({
+	ob: ob
+      });
+
+      this.errors.push('Stop ' + ob.id + ' not connected to route way, could not find nearest location (not rendered!)');
+    }
+  }
 }
 
 /**
