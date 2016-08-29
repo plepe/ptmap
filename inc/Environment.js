e0c1f3c7baf701db93342a69c8d7837f953b0e62
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2016-08-29 16:18:46 +0200

    Environment: every 2sec update rendering (if necessary); update time

--- inc/Environment.js
+++ inc/Environment.js
@@ -1,6 +1,13 @@
 function Environment() {
+  window.setInterval(this.update.bind(this), 2000);
+}
+
+Environment.prototype.update = function() {
+  this.current_date = new Date();
+
+  update_map_render_update_needed(function() {});
 }
 
 Environment.prototype.date = function() {
-  return new Date();
+  return this.current_date;
 }
