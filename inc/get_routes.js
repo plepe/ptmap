3f0fbdc31243d8b945596bbfefe6cd41e228ca1e
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2016-08-30 10:45:21 +0200

    Get Routes: remove cache handling -> moved to Overpass

--- inc/get_routes.js
+++ inc/get_routes.js
@@ -1,34 +1,15 @@
-var get_routes_cache = {};
-
 function get_routes(bounds, feature_callback, final_callback) {
   var query = [];
   for(var type in conf.routes) {
     query.push(overpass_regexp_escape(type));
   }
 
-  tile_bounds = bounds_to_tile(bounds);
-
   var routes = [];
   var visible_routes = [];
-  var cache_id = tile_bounds.toBBoxString() + '|' + query.join('|');
-
-  if(cache_id in get_routes_cache) {
-    return async.setImmediate(function() {
-      var routes = get_routes_cache[cache_id];
-
-      for(var i = 0; i < routes.length; i++)
-        if(routes[i].is_visible(bounds) && routes[i].is_active()) {
-          visible_routes.push(routes[i]);
-          feature_callback(null, routes[i]);
-        }
-
-      final_callback(null, visible_routes);
-    });
-  }
 
   overpass_bbox_query(
     'relation[type=route][route~"^(' + query.join('|') + ')$"];',
-    tile_bounds,
+    bounds,
     {
       priority: 2,
       order_approx_route_length: true
@@ -42,8 +23,6 @@ function get_routes(bounds, feature_callback, final_callback) {
       routes.push(route);
     },
     function(err, results) {
-      get_routes_cache[cache_id] = routes;
-
       final_callback(null, visible_routes);
     }
   );
