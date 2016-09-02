60b2066a4b4fb516326740bfcaccc5daa9bdbb70
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2016-09-02 11:24:59 +0200

    Clock: show current date in lower left corner

--- inc/Environment.js
+++ inc/Environment.js
@@ -1,9 +1,15 @@
 function Environment() {
   window.setInterval(this.update.bind(this), 2000);
+
+  this.div_clock = document.createElement('div');
+  this.div_clock.id = 'clock';
+  document.body.appendChild(this.div_clock);
 }
 
 Environment.prototype.update = function() {
   this.current_date = new Date();
+
+  this.div_clock.innerHTML = this.current_date.toISOString().slice(0, 16).replace('T', ' ');
 
   update_map_render_update_needed(function() {});
 }
