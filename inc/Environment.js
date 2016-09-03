6c2475790c07838235dfb8b23007a545a473aa4d
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2016-09-03 22:10:23 +0200

    Environment: every full minute update the map

--- inc/Environment.js
+++ inc/Environment.js
@@ -11,7 +11,11 @@ function Environment() {
 }
 
 Environment.prototype.update = function() {
+  var last_date = this.current_date;
   this.current_date = new Date(this.base_date.getTime() + new Date().getTime() - this.base_timestamp.getTime());
+
+  if(!last_date || last_date.toISOString().substr(0, 16) != this.current_date.toISOString().substr(0, 16))
+    check_update_map();
 
   this.div_clock.innerHTML = moment(this.current_date).format('llll');
 
