b64cfc69304a501d580c7e4c2635edec7c300d80
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2016-09-03 22:05:23 +0200

    Environment: base date to be shown on current date

--- inc/Environment.js
+++ inc/Environment.js
@@ -3,12 +3,15 @@ function Environment() {
   this.div_clock.id = 'clock';
   document.body.appendChild(this.div_clock);
 
+  this.base_date = new Date();
+  this.base_timestamp = new Date();
+
   this.update();
   window.setInterval(this.update.bind(this), 2000);
 }
 
 Environment.prototype.update = function() {
-  this.current_date = new Date();
+  this.current_date = new Date(this.base_date.getTime() + new Date().getTime() - this.base_timestamp.getTime());
 
   this.div_clock.innerHTML = moment(this.current_date).format('llll');
 
