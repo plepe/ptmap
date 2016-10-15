1ed0804527b6af33c75cc9f5e5ed828e98d75fe9
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2016-10-15 22:25:20 +0200

    Route: initial load way members

--- src/Route.js
+++ src/Route.js
@@ -1,7 +1,58 @@
+/* global overpassFrontend:false */
+var OverpassFrontend = require('overpass-frontend')
+
 function Route (object) {
   this.object = object
 
   this.id = this.object.id
+}
+
+Route.prototype.routeWays = function (bbox, callback) {
+  var wayIds = []
+  var wayIndexList = []
+  var wayIndex = 0
+  var init = false
+
+  if (typeof this._routeWays === 'undefined') {
+    this._routeWays = []
+    init = true
+  }
+
+  for (var i = 0; i < this.object.members.length; i++) {
+    var member = this.object.members[i]
+
+    if (member.type === 'way' && ['', 'forward', 'backward'].indexOf(member.role) !== -1) {
+      if (init) {
+        this._routeWays.push({
+          role: member.role,
+          way: false,
+          link: false
+        })
+      }
+
+      if (!this._routeWays[wayIndex].way) {
+        wayIds.push(member.id)
+        wayIndexList.push(wayIndex)
+      }
+
+      wayIndex++
+    }
+  }
+
+  overpassFrontend.get(wayIds,
+    {
+      bbox: bbox,
+      properties: OverpassFrontend.GEOM
+    },
+    function (wayIndexList, err, result, index) {
+      wayIndex = wayIndexList[index]
+
+      this._routeWays[wayIndex].way = result
+    }.bind(this, wayIndexList),
+    function (err) {
+      callback(err, this._routeWays)
+    }.bind(this)
+  )
 }
 
 module.exports = Route
--- test/test.js
+++ test/test.js
@@ -2,9 +2,11 @@ var fs = require('fs')
 var conf = JSON.parse(fs.readFileSync('test/conf.json', 'utf8'))
 
 var assert = require('assert')
+var async = require('async')
+var BoundingBox = require('boundingbox')
 
 var OverpassFrontend = require('overpass-frontend')
-var overpassFrontend = new OverpassFrontend(conf.url)
+GLOBAL.overpassFrontend = new OverpassFrontend(conf.url)
 
 var Route = require('../src/Route')
 var routes = {}
@@ -15,7 +17,7 @@ describe('Route', function () {
   it('load object', function (done) {
     overpassFrontend.get(['r1306478', 'r5333483'],
       {
-        properties: OverpassFrontend.ALL
+        properties: OverpassFrontend.TAGS | OverpassFrontend.MEMBERS | OverpassFrontend.BBOX
       },
       function (err, result, index) {
         if (err) {
@@ -23,6 +25,25 @@ describe('Route', function () {
         }
 
         routes[result.id] = new Route(result)
+      },
+      function (err) {
+        done(err)
+      }
+    )
+  })
+
+  it('load way members', function (done) {
+    async.each(routes,
+      function (route, callback) {
+        route.routeWays(new BoundingBox({
+          minlon: 16.336,
+          minlat: 48.195,
+          maxlon: 16.342,
+          maxlat: 48.202
+        }),
+        function (err, result) {
+          callback(err)
+        })
       },
       function (err) {
         done(err)
