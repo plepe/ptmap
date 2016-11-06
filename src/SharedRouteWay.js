65b6296cf3528b1260db9710cb5805e9dac89886
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2016-11-06 21:57:48 +0100

    SharedRouteWay: render route refs

--- src/SharedRouteWay.js
+++ src/SharedRouteWay.js
@@ -1,3 +1,5 @@
+var natsort = require('natsort')
+
 var sharedRouteWays = {}
 
 function SharedRouteWay (way) {
@@ -20,6 +22,77 @@ SharedRouteWay.prototype.routes = function () {
   }
 
   return ret
+}
+
+SharedRouteWay.prototype.build_label = function() {
+  var ret = '';
+  var routes = this.routes();
+  var ref_both = [];
+  var ref_forward = [];
+  var ref_backward = [];
+  var ref_unknown = [];
+
+
+  if(!this.links.length)
+    return '';
+
+  for(var i = 0; i < this.links.length; i++) {
+    var link = this.links[i];
+    var route = link.route;
+    var ref = null;
+
+    if('ref' in route.object.tags)
+      ref = route.object.tags.ref;
+
+    if(ref !== null) {
+      if(link.dir == null) {
+	if(ref_unknown.indexOf(ref) == -1)
+	  ref_unknown.push(ref);
+      }
+      else if(ref_both.indexOf(ref) != -1) {
+	// already seen in both directions -> ignore
+      }
+      else if(link.dir == 'backward') {
+	if(ref_forward.indexOf(ref) != -1) {
+	  ref_forward.splice(ref_forward.indexOf(ref), 1);
+	  ref_both.push(ref);
+	}
+	else if(ref_backward.indexOf(ref) == -1) {
+	  ref_backward.push(ref);
+	}
+      }
+      else if(link.dir == 'forward') {
+	if(ref_backward.indexOf(ref) != -1) {
+	  ref_backward.splice(ref_backward.indexOf(ref), 1);
+	  ref_both.push(ref);
+	}
+	else if(ref_forward.indexOf(ref) == -1) {
+	  ref_forward.push(ref);
+	}
+      }
+    }
+  }
+
+  var sort_param = { insensitive: true };
+  ref_both.sort(natsort(sort_param));
+  ref_forward.sort(natsort(sort_param));
+  ref_backward.sort(natsort(sort_param));
+  ref_unknown.sort(natsort(sort_param));
+
+  ret = '   ';
+  if(ref_backward.length)
+    ret += ' ← ' + ref_backward.join(', ') + '   ';
+
+  if(ref_both.length)
+    ret += ref_both.join(', ') + '   ';
+
+  if(ref_forward.length)
+    ret += ref_forward.join(', ') + ' → ';
+
+  if(ref_unknown.length)
+    ret += ' ?? ' + ref_unknown.join(', ') + ' ?? ';
+
+  return ret + '             ';
 }
 
 SharedRouteWay.prototype.update = function () {
@@ -46,6 +119,14 @@ SharedRouteWay.prototype.show = function (map) {
       opacity: 1
     }).addTo(map)
   }
+
+  this.feature.setText(this.build_label(), {
+    repeat: true,
+    offset: 12,
+    attributes: {
+      fill: route_conf.color
+    }
+  });
 
   this.shown = true
 }
